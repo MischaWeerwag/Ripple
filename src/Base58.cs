fd567f66032d50b5b927cf9693ec5fecdeeb9d5b
Fraser Waters <frassle@gmail.com>
2020-03-04 22:39:17 +0000

    Split Base58 into own file

---
+++ src/Base58.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Buffers;
+
+namespace Ibasa.Ripple
+{
+    // N.B compare to System.Buffers.Text.Base64 API
+
+    /// <summary>
+    /// Converts between binary data and UTF-8 encoded text that is represented in base 58.
+    /// </summary>
+    public static class Base58
+    {
+        private static string alphabet = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz";
+
+        private static int[] index;
+
+        static Base58()
+        {
+            index = new int[123];
+
+            for (int i = 0; i < index.Length; i++)
+            {
+                index[i] = -1;
+            }
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                index[alphabet[i]] = i;
+            }
+        }
+
+        public static void ConvertFrom(string base58, Span<byte> bytes)
+        {
+            if (base58.Length == 0)
+            {
+                return;
+            }
+
+            byte[] input58 = new byte[base58.Length];
+            // Transform the String to a base58 byte sequence
+            for (int i = 0; i < base58.Length; ++i)
+            {
+                char c = base58[i];
+
+                int digit58 = -1;
+                if (c >= 0 && c < 123)
+                {
+                    digit58 = index[c];
+                }
+                if (digit58 < 0)
+                {
+                    throw new Exception("Illegal character " + c + " at " + i);
+                }
+
+                input58[i] = (byte)digit58;
+            }
+            // Count leading zeroes
+            var zeroCount = 0;
+            while (zeroCount < input58.Length && input58[zeroCount] == 0)
+            {
+                ++zeroCount;
+            }
+            // The encoding
+            var temp = new byte[base58.Length];
+            var j = temp.Length;
+
+            var startAt = zeroCount;
+            while (startAt < input58.Length)
+            {
+                var mod = DivMod256(input58, startAt);
+                if (input58[startAt] == 0)
+                {
+                    ++startAt;
+                }
+
+                temp[--j] = mod;
+            }
+            // Do no add extra leading zeroes, move j to first non null byte.
+            while (j < temp.Length && temp[j] == 0)
+            {
+                ++j;
+            }
+
+            CopyOfRange(temp, j - zeroCount, temp.Length, bytes);
+        }
+        private static void CopyOfRange(byte[] source, int from, int to, Span<byte> dest)
+        {
+            new Span<byte>(source, from, to - from).CopyTo(dest);
+        }
+
+        private static byte DivMod256(byte[] number58, int startAt)
+        {
+            var remainder = 0;
+            for (var i = startAt; i < number58.Length; i++)
+            {
+                var digit58 = number58[i];
+                var temp = remainder * 58 + digit58;
+
+                number58[i] = (byte)(temp / 256);
+
+                remainder = temp % 256;
+            }
+
+            return (byte)remainder;
+        }
+
+        public static string ConvertTo(Span<byte> bytes)
+        {
+            if (bytes.Length == 0)
+            {
+                return "";
+            }
+            // Count leading zeroes.
+            int zeroCount = 0;
+            while (zeroCount < bytes.Length && bytes[zeroCount] == 0)
+            {
+                ++zeroCount;
+            }
+            // The actual encoding.
+            byte[] temp = new byte[bytes.Length * 2];
+            int j = temp.Length;
+
+            int startAt = zeroCount;
+            while (startAt < bytes.Length)
+            {
+                byte mod = DivMod58(bytes, startAt);
+                if (bytes[startAt] == 0)
+                {
+                    ++startAt;
+                }
+                temp[--j] = (byte)alphabet[mod];
+            }
+
+            // Strip extra '1' if there are some after decoding.
+            while (j < temp.Length && temp[j] == alphabet[0])
+            {
+                ++j;
+            }
+            // Add as many leading '1' as there were leading zeros.
+            while (--zeroCount >= 0)
+            {
+                temp[--j] = (byte)alphabet[0];
+            }
+
+            var output = new byte[temp.Length - j];
+            CopyOfRange(temp, j, temp.Length, output);
+            return System.Text.Encoding.ASCII.GetString(output);
+        }
+        private static byte DivMod58(Span<byte> number, int startAt)
+        {
+            var remainder = 0;
+            for (var i = startAt; i < number.Length; i++)
+            {
+                var digit256 = number[i];
+                var temp = remainder * 256 + digit256;
+
+                number[i] = (byte)(temp / 58);
+
+                remainder = temp % 58;
+            }
+
+            return (byte)remainder;
+        }
+    }
+
+    public static class Base58Check
+    {
+        [ThreadStatic]
+        static System.Security.Cryptography.SHA256 sha256 = System.Security.Cryptography.SHA256.Create();
+
+        public static void ConvertFrom(string base58, Span<byte> bytes)
+        {
+            Span<byte> buffer = stackalloc byte[bytes.Length + 4];
+            Base58.ConvertFrom(base58, buffer);
+
+            Span<byte> firstHash = stackalloc byte[32];
+
+            if (!sha256.TryComputeHash(buffer.Slice(0, bytes.Length), firstHash, out var written) || written != 32)
+            {
+                throw new Exception("sha256 error");
+            }
+
+            Span<byte> secondHash = stackalloc byte[32];
+
+            if (!sha256.TryComputeHash(firstHash, secondHash, out written) || written != 32)
+            {
+                throw new Exception("sha256 error");
+            }
+
+            if (!buffer.Slice(bytes.Length, 4).SequenceEqual(secondHash.Slice(0, 4)))
+            {
+                throw new Exception("hash code did not match");
+            }
+
+            buffer.Slice(0, bytes.Length).CopyTo(bytes);
+        }
+
+        public static string ConvertTo(Span<byte> bytes)
+        {
+            Span<byte> buffer = stackalloc byte[bytes.Length + 4];
+            bytes.CopyTo(buffer);
+
+            Span<byte> firstHash = stackalloc byte[32];
+
+            if (!sha256.TryComputeHash(buffer.Slice(0, bytes.Length), firstHash, out var written) || written != 32)
+            {
+                throw new Exception("sha256 error");
+            }
+
+            Span<byte> secondHash = stackalloc byte[32];
+
+            if (!sha256.TryComputeHash(firstHash, secondHash, out written) || written != 32)
+            {
+                throw new Exception("sha256 error");
+            }
+
+            secondHash.Slice(0, 4).CopyTo(buffer.Slice(bytes.Length, 4));
+
+            return Base58.ConvertTo(buffer);
+        }
+    }
+}

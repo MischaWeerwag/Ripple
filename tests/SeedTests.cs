cc95ed8c90f9d0141699bfb5047739ca5eaca383
Fraser Waters <frassle@gmail.com>
2020-03-21 22:25:45 +0000

    Few more simple seed tests

--- tests/SeedTests.cs
+++ tests/SeedTests.cs
@@ -1,17 +1,44 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Ibasa.Ripple.Tests
 {
     public class SeedTests
     {
+        public static IEnumerable<object[]> seeds
+        {
+            get
+            {
+                yield return new object[] { "snoPBrXtMeMyMHUVTgbuqAfg1SUTb" };
+                yield return new object[] { "sp6JS7f14BuwFY8Mw6bTtLKWauoUs" };
+            }
+        }
+
         [Theory]
-        [InlineData("snoPBrXtMeMyMHUVTgbuqAfg1SUTb")]
-        [InlineData("sp6JS7f14BuwFY8Mw6bTtLKWauoUs")]
+        [MemberData(nameof(seeds))]
         public void TestRoundTrip(string base58)
         {
             var seed = new Seed(base58);
             Assert.Equal(base58, seed.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(seeds))]
+        public void TestCopy(string base58)
+        {
+            var seed = new Seed(base58);
+            var copy = seed;
+            Assert.Equal(seed.ToString(), copy.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(seeds))]
+        public void TestBoxing(string base58)
+        {
+            var seed = new Seed(base58);
+            var copy = (object)seed;
+            Assert.Equal(seed.ToString(), copy.ToString());
         }
 
         [Fact]

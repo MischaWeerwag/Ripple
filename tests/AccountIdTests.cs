6117f32ac4b604893df4262b9fc534206844b73c
Fraser Waters <frassle@gmail.com>
2020-03-21 16:50:26 +0000

    AccountId tests

--- tests/AccountIdTests.cs
+++ tests/AccountIdTests.cs
@@ -1,19 +1,46 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Ibasa.Ripple.Tests
 {
     public class AccountIdTests
     {
+        public static IEnumerable<object[]> accounts
+        {
+            get
+            {
+                yield return new object[] { "r4nmanwKSE6GpkTCrBjz8uanrGZabbpSfp" };
+                yield return new object[] { "r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV" };
+                yield return new object[] { "rEopG7qc7ZWFMvCSH3GYeJJ7GPAQnKmxgw" };
+                yield return new object[] { "rrrrrrrrrrrrrrrrrrrrrhoLvTp" };
+            }
+        }
+
         [Theory]
-        [InlineData("r4nmanwKSE6GpkTCrBjz8uanrGZabbpSfp")]
-        [InlineData("r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV")]
-        [InlineData("rEopG7qc7ZWFMvCSH3GYeJJ7GPAQnKmxgw")]
-        [InlineData("rrrrrrrrrrrrrrrrrrrrrhoLvTp")]
+        [MemberData(nameof(accounts))]
         public void TestRoundTrip(string base58)
         {
             var account = new AccountId(base58);
             Assert.Equal(base58, account.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(accounts))]
+        public void TestCopy(string base58)
+        {
+            var account = new AccountId(base58);
+            var copy = account;
+            Assert.Equal(account.ToString(), copy.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(accounts))]
+        public void TestBoxing(string base58)
+        {
+            var account = new AccountId(base58);
+            var copy = (object)account;
+            Assert.Equal(account.ToString(), copy.ToString());
         }
     }
 }

9b55b8eadda213624066774a695fcb922d0eaa45
Fraser Waters <frassle@gmail.com>
2020-03-27 22:32:24 +0000

    Split Epoch into own file

--- src/Domain.cs
+++ src/Domain.cs
@@ -490,27 +490,6 @@ namespace Ibasa.Ripple
         //    type: "response"
     }
 
-    public static class Epoch
-    {
-        private static DateTime epoch = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-        public static uint FromDateTime(DateTime dateTime)
-        {
-            var utc = dateTime.ToUniversalTime();
-            if (utc < epoch)
-            {
-                throw new ArgumentOutOfRangeException("dateTime", "dateTime is before the ripple epoch of 2000-01-01");
-            }
-
-            return (uint)(utc.Subtract(epoch).TotalSeconds);
-        }
-        public static DateTime ToDateTime(uint timestamp)
-        {
-            return epoch.AddSeconds(timestamp);
-        }
-    }
-
-
     public sealed class LedgerResponse
     {
         public Hash256 LedgerHash { get; private set; }
---
+++ src/Epoch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ibasa.Ripple
+{
+    public static class Epoch
+    {
+        private static DateTime epoch = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static uint FromDateTime(DateTime dateTime)
+        {
+            var utc = dateTime.ToUniversalTime();
+            if (utc < epoch)
+            {
+                throw new ArgumentOutOfRangeException("dateTime", "dateTime is before the ripple epoch of 2000-01-01");
+            }
+
+            return (uint)(utc.Subtract(epoch).TotalSeconds);
+        }
+
+        public static DateTime ToDateTime(uint timestamp)
+        {
+            return epoch.AddSeconds(timestamp);
+        }
+    }
+}

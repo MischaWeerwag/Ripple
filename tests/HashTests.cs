6fe76138e237cba9dcf72f1b1cf38ad22c177ab5
Fraser Waters <frassle@gmail.com>
2020-03-27 23:11:12 +0000

    Hash128 tests

--- tests/HashTests.cs
+++ tests/HashTests.cs
@@ -21,4 +21,23 @@ namespace Ibasa.Ripple.Tests
             Assert.Equal(hex, hash.ToString());
         }
     }
+    public class Hash128Tests
+    {
+        [Fact]
+        public void TestDefault()
+        {
+            var hash = new Hash128();
+            Assert.Equal("00000000000000000000000000000000", hash.ToString());
+        }
+
+        [Theory]
+        [InlineData("00000000000000000000000000000000")]
+        [InlineData("C76E6A8133EF59F04F9FC1AAA70E41AF")]
+        [InlineData("8ED765AEBBD6767603C2C9375B2679AE")]
+        public void TestRoundTrip(string hex)
+        {
+            var hash = new Hash128(hex);
+            Assert.Equal(hex, hash.ToString());
+        }
+    }
 }

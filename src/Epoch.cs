0adcac9d1ea1ffa3df77b07da3c8c924ae8eac1e
Fraser Waters <frassle@gmail.com>
2020-04-04 22:56:46 +0100

    Update Epoch to also work with DateTimeOffset

--- src/Epoch.cs
+++ src/Epoch.cs
@@ -4,20 +4,35 @@ namespace Ibasa.Ripple
 {
     public static class Epoch
     {
-        private static DateTime epoch = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static DateTimeOffset epoch = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
 
         public static uint FromDateTime(DateTime dateTime)
         {
             var utc = dateTime.ToUniversalTime();
             if (utc < epoch)
             {
-                throw new ArgumentOutOfRangeException("dateTime", "dateTime is before the ripple epoch of 2000-01-01");
+                throw new ArgumentOutOfRangeException("dateTime", dateTime, "dateTime is before the ripple epoch of 2000-01-01");
+            }
+
+            return (uint)(utc.Subtract(epoch.UtcDateTime).TotalSeconds);
+        }
+
+        public static DateTime ToDateTime(uint timestamp)
+        {
+            return epoch.AddSeconds(timestamp).UtcDateTime;
+        }
+        public static uint FromDateTimeOffset(DateTimeOffset dateTimeOffset)
+        {
+            var utc = dateTimeOffset.ToUniversalTime();
+            if (utc < epoch)
+            {
+                throw new ArgumentOutOfRangeException("dateTimeOffset", dateTimeOffset, "dateTimeOffset is before the ripple epoch of 2000-01-01");
             }
 
             return (uint)(utc.Subtract(epoch).TotalSeconds);
         }
 
-        public static DateTime ToDateTime(uint timestamp)
+        public static DateTimeOffset ToDateTimeOffset(uint timestamp)
         {
             return epoch.AddSeconds(timestamp);
         }
--- tests/EpochTests.cs
+++ tests/EpochTests.cs
@@ -10,13 +10,22 @@ namespace Ibasa.Ripple.Tests
         {
             Assert.Equal(new DateTime(2000, 1, 1), Epoch.ToDateTime(0));
             Assert.Equal(0u, Epoch.FromDateTime(new DateTime(2000, 1, 1)));
+            Assert.Equal(new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero), Epoch.ToDateTimeOffset(0));
+            Assert.Equal(0u, Epoch.FromDateTimeOffset(new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero)));
         }
 
         [Fact]
         public void TestIsUtc()
         {
-            var datetime = Epoch.ToDateTime(0);
-            Assert.Equal(DateTimeKind.Utc, datetime.Kind);
+            var dateTime = Epoch.ToDateTime(0);
+            Assert.Equal(DateTimeKind.Utc, dateTime.Kind);
+        }
+
+        [Fact]
+        public void TestIsZeroOffset()
+        {
+            var dateTimeOffset = Epoch.ToDateTimeOffset(0);
+            Assert.Equal(TimeSpan.Zero, dateTimeOffset.Offset);
         }
 
         [Theory]
@@ -25,18 +34,38 @@ namespace Ibasa.Ripple.Tests
         [InlineData(1U)]
         [InlineData(4294967294U)]
         [InlineData(2147483647U)]
-        public void TestRoundTrip(uint timestamp)
+        public void TestDateTimeRoundTrip(uint timestamp)
         {
             Assert.Equal(timestamp, Epoch.FromDateTime(Epoch.ToDateTime(timestamp)));
+        }
+
+        [Theory]
+        [InlineData(uint.MinValue)]
+        [InlineData(uint.MaxValue)]
+        [InlineData(1U)]
+        [InlineData(4294967294U)]
+        [InlineData(2147483647U)]
+        public void TestDateTimeOffsetRoundTrip(uint timestamp)
+        {
+            Assert.Equal(timestamp, Epoch.FromDateTimeOffset(Epoch.ToDateTimeOffset(timestamp)));
         }
 
         [Fact]
         public void TestPreEphoch()
         {
-            var exc = Assert.Throws<ArgumentOutOfRangeException>(() =>
-                Epoch.FromDateTime(new DateTime(1999, 12, 30)));
+            ArgumentOutOfRangeException exc;
+
+            var dateTime = new DateTime(1999, 12, 30);
+            exc = Assert.Throws<ArgumentOutOfRangeException>(() => Epoch.FromDateTime(dateTime));
             Assert.Equal("dateTime", exc.ParamName);
-            Assert.Equal("dateTime is before the ripple epoch of 2000-01-01 (Parameter 'dateTime')", exc.Message);
+            Assert.Equal(dateTime, exc.ActualValue);
+            Assert.Equal("dateTime is before the ripple epoch of 2000-01-01 (Parameter 'dateTime')\r\nActual value was 30/12/1999 00:00:00.", exc.Message);
+
+            var dateTimeOffset = new DateTimeOffset(1999, 12, 30, 0, 0, 0, TimeSpan.Zero);
+            exc = Assert.Throws<ArgumentOutOfRangeException>(() => Epoch.FromDateTimeOffset(dateTimeOffset));
+            Assert.Equal("dateTimeOffset", exc.ParamName);
+            Assert.Equal(dateTimeOffset, exc.ActualValue);
+            Assert.Equal("dateTimeOffset is before the ripple epoch of 2000-01-01 (Parameter 'dateTimeOffset')\r\nActual value was 30/12/1999 00:00:00 +00:00.", exc.Message);
         }
     }
 }

85280ebcd9394ba35458ec838c147628f107a10a
Fraser Waters <frassle@gmail.com>
2021-02-07 16:35:31 +0000

    Move docker tests

--- tests/AdminTests.cs
+++ tests/AdminTests.cs
@@ -10,19 +10,6 @@ using System.Collections.Generic;
 
 namespace Ibasa.Ripple.Tests
 {
-    public class DockerDotNetTests
-    {
-        [Fact(Skip = "Causing issues on CI")]
-        public async Task SanityCheck()
-        {
-            var client = new DockerClientConfiguration().CreateClient();
-            var version = await client.System.GetVersionAsync();
-            Assert.NotNull(version.KernelVersion);
-            Assert.NotNull(version.APIVersion);
-            Assert.NotNull(version.Version);
-        }
-    }
-
     public abstract class AdminTestsSetup : IDisposable
     {
         private static string config = @"
